0276529fd4f005d077502b88e59766237c6cf344
Chase-333 <chaseraymondwimpy@gmail.com>
2022-08-06 04:31:59 -0400

    huge!

--- FNF-PsychEngine-main/assets/preload/stages/hospital.lua
+++ FNF-PsychEngine-main/assets/preload/stages/hospital.lua
@@ -1,10 +1,57 @@
 function onCreate()
 	
-	makeLuaSprite('hospitan', 'hospitan', -170, 0); 
-	setScrollFactor('hospitan', 1, 1);
-	scaleObject('hospitan', 1, 1);
+	makeLuaSprite('sky', 'sky', -150, 0); 
+	setScrollFactor('sky', 1, 1);
+	scaleObject('sky', 1, 1);
 
-	addLuaSprite('hospitan', false);
+	makeLuaSprite('building2', 'building2', 80, 200); 
+	setScrollFactor('building2', 1.9, 1.2);
+	scaleObject('building2', 1, 1);
+
+	makeLuaSprite('building1', 'building1', 80, 290); 
+	setScrollFactor('building1', 1.6, 1.1);
+	scaleObject('building1', 1, 1);
+
+	makeLuaSprite('room', 'room', -150, 0); 
+	setScrollFactor('room', 1, 1);
+	scaleObject('room', 1, 1);
+
+	makeLuaSprite('iv bag', 'iv bag', -50, 300); 
+	setScrollFactor('iv bag', 1.2, 1);
+	scaleObject('iv bag', 1, 1);
+
+	makeLuaSprite('monitor', 'monitor', 0, 300); 
+	setScrollFactor('monitor', 1.1, 1);
+	scaleObject('monitor', 1, 1);
+
+	makeLuaSprite('desk', 'desk', 1300, 350); 
+	setScrollFactor('desk', 1.1, 1);
+	scaleObject('desk', 1, 1);
+
+	makeLuaSprite('bed', 'bed', -100, 600); 
+	setScrollFactor('bed', 1.1, 1);
+	scaleObject('bed', 1, 1);
+
+	addLuaSprite('sky', false);
+	addLuaSprite('building2', false);
+	addLuaSprite('building1', false);
+	addLuaSprite('room', false);
+	addLuaSprite('monitor', false);
+	addLuaSprite('iv bag', false);
+	addLuaSprite('desk', false);
+	addLuaSprite('bed', false);
 
 	close(true); --For performance reasons
+end
+
+function onBeatHit()
+	
+end
+
+function onStepHit()
+	
+end
+
+function onUpdate()
+	
 end
---
+++ FNF-PsychEngine-main/assets/preload/stages/purgatory.lua
@@ -0,0 +1,30 @@
+function onCreate()
+	
+	makeLuaSprite('PurgatorySky', 'PurgatorySky', -490, -150); 
+	setScrollFactor('PurgatorySky', 1.3, 1.2);
+	scaleObject('PurgatorySky', 1.2, 1.2);
+
+	makeLuaSprite('RockBack', 'RockBack', -400, 150); 
+	setScrollFactor('RockBack', 1.5, 0.9);
+	scaleObject('RockBack', 1, 1);
+
+	makeLuaSprite('Platform', 'Platform', -600, 300); 
+	setScrollFactor('Platform', 1, 1);
+	scaleObject('Platform', 1.2, 1.2);
+
+	makeLuaSprite('RockL', 'RockL', -400, 300); 
+	setScrollFactor('RockL', 1, 0.9);
+	scaleObject('RockL', 1, 1);
+
+	makeLuaSprite('RockR', 'RockR', -400, 300); 
+	setScrollFactor('RockR', 1, 0.9);
+	scaleObject('RockR', 1, 1);
+
+	addLuaSprite('PurgatorySky', false);
+	addLuaSprite('RockBack', false);
+	addLuaSprite('Platform', false);
+	addLuaSprite('RockL', true);
+	addLuaSprite('RockR', true);
+
+	close(true); --For performance reasons
+end
